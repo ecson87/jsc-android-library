091d63f4f735534f0e6137fedbb362e6e4a4a591
Erwin Cesar <ecson@morason.id>
2022-02-16 14:12:11 +0700

    Initial commit

---
+++ composable-widget/src/main/java/id/go/jakarta/smartcity/android/android/library/composable_widget/HelloCompose.kt
@@ -0,0 +1,31 @@
+package id.go.jakarta.smartcity.android.android.library.composable_widget
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HelloCompose() {
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Text(
+      text = "Welcome",
+    )
+    Text(
+      text = "to",
+    )
+    Text(
+      text = "Jakarta Smart City",
+    )
+    Text(
+      text = "Android Library",
+    )
+  }
+}
